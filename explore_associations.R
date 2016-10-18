8320919a698c6fa82362e5191cba064fd02c3d38
jvwowd <jasonvwoude@gmail.com>
2016-10-18 15:00:57 -0500

    Replaced explore_associations.R
    Craig sent a new version of explore_associations.R

--- explore_associations.R
+++ explore_associations.R
@@ -1,13 +1,13 @@
-wd <- getwd()
-setwd("Accessory code/Active-inactive-essential gold calls and analysis")
-source("Clean_matt_calls.R")
-setwd(wd)
-setwd("Figure 1")
-source("code.R")
-setwd(wd)
+source("Aij_utility_funcs.R")
+source("valContext.R")
+context <- get.wrapped.aij.context.from.ds("Hope Ecoli")
+eijs <- get.expression.data.outliersNA(context)
+aijs <- MultiMM_NBF(context)
 load("nonCompleteExpts_hope_ecoli.Rdata")
+source("expnameMaps.R")
+expnameMap <- get.expname.map("inputs/Ecoli_Cel_to_chip_fromClaire_edited.tab")
 
-is.complete <- function(expname) !(get_chipname(expname) %in% nonCompleteExpts)
+is.complete <- function(expname) !(get_chipname(expnameMap, expname) %in% nonCompleteExpts)
 
 myf <- function(numIn, numAc) fisher.test(matrix(c(numIn, numAc, length(offexps)-numIn, length(onexps)-numAc), nrow=2, ncol=2))$p
 
@@ -15,13 +15,13 @@ efd <- read.delim("inputs/E_coli_v4_Build_6_6-23-15.experiment_feature_descripti
 
 get.counts <- function(feature) {
   inactive <- ifelse(length(offexps)==0, 0, sum(sapply(offexps, function(offexp) {
-    feature %in% efd[efd$experiment_name==get_chipname(offexp),]$feature_name
+    feature %in% efd[efd$experiment_name==get_chipname(expnameMap, offexp),]$feature_name
   })))
   active <- ifelse(length(onexps)==0, 0, sum(sapply(onexps, function(onexp) {
-    feature %in% efd[efd$experiment_name==get_chipname(onexp),]$feature_name
+    feature %in% efd[efd$experiment_name==get_chipname(expnameMap, onexp),]$feature_name
   })))
   unknown <- ifelse(length(unkexps)==0, 0, sum(sapply(unkexps, function(unkexp) {
-    feature %in% efd[efd$experiment_name==get_chipname(unkexp),]$feature_name
+    feature %in% efd[efd$experiment_name==get_chipname(expnameMap, unkexp),]$feature_name
   })))
   return(c(inactive, active, unknown))
 }
@@ -43,9 +43,9 @@ contingency.table <- function(feature) {
 }
 
 all.tests <- function(pegnum, aijs) {
-  offexps <<- colnames(remove_CEL_gz(eijs))[aijs[gene(pegnum),]<0.2]
-  onexps <<- colnames(remove_CEL_gz(eijs))[aijs[gene(pegnum),]>0.8]
-  unkexps <<- colnames(remove_CEL_gz(eijs))[aijs[gene(pegnum),]>=0.2 & aijs[gene(pegnum),]<=0.8]
+  offexps <<- colnames(remove_CEL_gz(eijs))[aijs[hope.gene(pegnum),]<0.2]
+  onexps <<- colnames(remove_CEL_gz(eijs))[aijs[hope.gene(pegnum),]>0.8]
+  unkexps <<- colnames(remove_CEL_gz(eijs))[aijs[hope.gene(pegnum),]>=0.2 & aijs[hope.gene(pegnum),]<=0.8]
   
   if(length(offexps)==0) {
     cat("No inactive experiments\n")
